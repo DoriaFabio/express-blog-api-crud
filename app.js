75433b68c8531a02b5792ee4201ac7f4d4d8efc9
DoriaFabio <doriafabio2001@gmail.com>
2025-01-09 13:45:05 +0000

    add corspolicy

--- app.js
+++ app.js
@@ -2,28 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 5500;
 
+// middlewares
+const corsPolicy = require("./middlewares/corsPolicy");
+const errorsHandler = require("./middlewares/errorsHandler");
+const notFound = require("./middlewares/notFound");
+const postsRouter = require("./routers/posts");
 //definisco dove sono gli asset statici
 app.use(express.static("public")); //http://localhost:5500/
+
+app.use(corsPolicy);
 
 // il body di qualunque richiesta va parsato come application/json
 app.use(express.json());
 
+//rotte API
+app.use("/posts", postsRouter);
+
+app.use(errorsHandler);
+
+app.use(notFound);
+
 //rotte web
 // const homeController = require("./controllers/homeController");
 // app.get("/", homeController.index);
-
-//rotte API
-const postsRouter = require("./routers/posts");
-app.use("/posts", postsRouter);
-
-// middlewares
-const checkTime = require("./middlewares/checkTime");
-const errorsHandler = require("./middlewares/errorsHandler");
-const notFound = require("./middlewares/notFound");
-
-app.use(checkTime);
-app.use(errorsHandler);
-app.use(notFound);
 
 //rotta fallback
 // app.all("*", (req, res) => {
