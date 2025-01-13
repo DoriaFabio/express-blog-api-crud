5f3abeaf91c40bf25462c86091c621e1fdc708b8
DoriaFabio <doriafabio2001@gmail.com>
2025-01-13 00:06:50 +0000

    modify data server

--- data/post.js
+++ data/post.js
@@ -1,48 +1,90 @@
 const tags = [
-    { id: 1, title: "farina" },
-    { id: 2, title: "dolci" },
-    { id: 3, title: "salato" },
-    { id: 4, title: "pane" },
-    { id: 5, title: "pasta" },
-    { id: 6, title: "pizza" },
+    { id: 1, title: "erba" },
+    { id: 2, title: "fuoco" },
+    { id: 3, title: "acqua" },
+    { id: 4, title: "veleno" },
+    { id: 5, title: "lotta" },
+    { id: 6, title: "roccia" },
+    { id: 7, title: "coleottero" },
+    { id: 8, title: "drago" },
+    { id: 9, title: "folletto" },
+    { id: 10, title: "spettro" },
+    { id: 11, title: "terra" },
+    { id: 12, title: "normale" },
+    { id: 13, title: "buio" },
+    { id: 14, title: "elettro" },
+    { id: 15, title: "volante" },
+    { id: 16, title: "acciaio" },
+    { id: 17, title: "ghiaccio" },
+    { id: 18, title: "psico" },
+
 ];
 
 const posts = [
     {
         id: 1,
-        titolo: "Ciambellone",
-        contenuto: "Descrizione del ciambellone",
-        immagine: "images/ciambellone.jpeg",
-        tags: ["ciambellone", "dolci", "cucina"]
+        titolo: "Bulbasaur",
+        contenuto: "Dopo la nascita, per un periodo di tempo cresce assorbendo i nutrienti stipati nel bulbo sul dorso.",
+        immagine: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/001.png",
+        tags: ["erba", "veleno"]
     },
     {
         id: 2,
-        titolo: "Cracker con barbabietola",
-        contenuto: "Descrizione cracker alla barbabietola",
-        immagine: "images/cracker_barbabietola.jpeg",
-        tags: ["cracker", "barbabietola", "spuntino"]
+        titolo: "Ivysaur",
+        contenuto: "Più si espone alla luce solare, più acquisisce energie facendo crescere il bocciolo che ha sul dorso.",
+        immagine: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/002.png",
+        tags: ["erba", "veleno"]
     },
     {
         id: 3,
-        titolo: "Pane fritto dolce",
-        contenuto: "Descrizione del pane fritto dolce",
-        immagine: "images/pane_fritto_dolce.jpeg",
-        tags: ["pane fritto", "dolci", "pane"]
+        titolo: "Venusaur",
+        contenuto: "Può convertire la luce solare in energia. Per questo è più forte in estate.",
+        immagine: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/003.png",
+        tags: ["erba", "veleno"]
     },
     {
         id: 4,
-        titolo: "Pasta con barbabietola",
-        contenuto: "Descrizione della pasta con la barbabietola",
-        immagine: "images/pasta_barbabietola.jpeg",
-        tags: ["pasta", "barbabietola", "farina"]
+        titolo: "Charmander",
+        contenuto: "La fiamma che ha sulla coda indica lo stato della sua forza vitale: quando è fiacco, la fiamma è fioca.",
+        immagine: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png",
+        tags: ["fuoco"]
     },
     {
         id: 5,
-        titolo: "Torta paesana",
-        contenuto: "Descrizione della torta paesana",
-        immagine: "images/torta_paesana.jpeg",
-        tags: ["torta", "dolci", "paesana"]
-    }
+        titolo: "Charmeleon",
+        contenuto: "Quando agita la coda infuocata, la temperatura circostante sale sempre di più, causando tormento ai suoi nemici.",
+        immagine: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/005.png",
+        tags: ["fuoco"]
+    },
+    {
+        id: 6,
+        titolo: "Charizard",
+        contenuto: "Se s’infuria, la fiamma che ha sulla punta della coda si colora di un blu biancastro.",
+        immagine: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/006.png",
+        tags: ["fuoco", "volante"]
+    },
+    {
+        id: 7,
+        titolo: "Squirtle",
+        contenuto: "Dopo la nascita, la sua schiena si gonfia e si trasforma in una dura corazza. Sputa una potente schiuma dalla bocca.",
+        immagine: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/007.png",
+        tags: ["acqua"]
+    },
+    {
+        id: 8,
+        titolo: "Wartortle",
+        contenuto: "La coda lunga e folta è simbolo di longevità. È un Pokémon particolarmente popolare tra le persone anziane.",
+        immagine: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/008.png",
+        tags: ["acqua"]
+    },
+    {
+        id: 9,
+        titolo: "Blastoise",
+        contenuto: "Aumenta di proposito il suo peso corporeo per resistere al contraccolpo dei getti d’acqua che spara.",
+        immagine: "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/009.png",
+        tags: ["acqua"]
+    },
+    
 ];
 
 export {posts, tags};
